04849d75e2f1fdae6122650c20401966114ed0f5
Martin Weingart <martinweingart@gmail.com>
2022-07-17 10:38:43 -0300

    Add constants module

---
+++ src/constants/defaults.js
@@ -0,0 +1,2 @@
+module.exports.LIST_MIN_DEFAULT = 0;
+module.exports.LIST_MAX_DEFAULT = 20;
---
+++ src/constants/errors.js
@@ -0,0 +1,9 @@
+module.exports.WRITE_FILE_ERROR = "Error while writing json file";
+
+module.exports.CONFIG_FILE_ERROR = "Error while reading configuration file";
+
+module.exports.MISSING_COLLECTIONS_CONFIG_ERROR =
+  "No collections definition found in configuration file";
+
+module.exports.MISSING_COLLECTION_NAME_ERROR =
+  "Missing collection name definition";
